0250de52e8d987991c7c6dacaca1ac6f51758747
matheusvirissimo <matheus.virissimo@unesp.br>
2023-11-29 21:34:31 -0300

    new sign-up page

--- web/signup.js
+++ web/signup.js
@@ -14,10 +14,86 @@ async function signUp(){
     logoNtitle.style.position = "absolute"
     logoNtitle.style.marginBottom = "27rem"
 
-    var registerArea = document.getElementById("div")
+    var registerArea = document.createElement("div")
     registerArea.setAttribute("id", "registerArea")
     registerArea.style.opacity = "0"
 
+    var form = document.createElement("form")
+    form.setAttribute("id", "registerForm")
+
+    var namesBox = document.createElement("namesBox")
+    namesBox.setAttribute("id", "namesBox")
+
+    var firstNameLabel = document.createElement("label")
+    firstNameLabel.setAttribute("id", "firstNameLabel")
+    firstNameLabel.innerHTML = "Nome:"
+
+    var secondNameLabel = document.createElement("label")
+    secondNameLabel.setAttribute("id", "secondNameLabel")
+    secondNameLabel.innerHTML = "Sobrenome:"
+    
+    var firstName = document.createElement("input")
+    firstName.setAttribute("id", "firstName")
+    firstName.setAttribute("type", "text")
+    firstName.setAttribute("placeholder", "Primeiro nome")
+
+    var secondName = document.createElement("input")
+    secondName.setAttribute("id", "secondName")
+    secondName.setAttribute("type", "text")
+    secondName.setAttribute("placeholder", "Sobrenome")
+
+    var emailLabel = document.createElement("label")
+    emailLabel.setAttribute("id", "emailLabel")
+    emailLabel.innerHTML = "Email: "
+
+    var email = document.createElement("input")
+    email.setAttribute("id", "email")
+    email.setAttribute("type", "text")
+    email.setAttribute("placeholder", "InsiraAquiSeu@Email.com")
+
+    var passwordLabel = document.createElement("label")
+    passwordLabel.setAttribute("id", "passwordLabel")
+    passwordLabel.innerHTML = "Senha: "
+
+    var password = document.createElement("input")
+    password.setAttribute("id", "password")
+    password.setAttribute("type", "password")
+    password.setAttribute("placeholder", "Digite a sua senha")
+
+    var confirmPasswordLabel = document.createElement("label")
+    confirmPasswordLabel.setAttribute("id", "confirmPasswordLabel")
+    confirmPasswordLabel.innerHTML = "Confirme sua senha: "
+
+    var confirmPassword = document.createElement("input")
+    confirmPassword.setAttribute("id", "confirmPassword")
+    confirmPassword.setAttribute("type", "password")
+    confirmPassword.setAttribute("placeholder", "Digite a sua senha novamente")
+
+    var loginButton = document.createElement("button");
+    loginButton.setAttribute("id", "loginButton")
+    loginButton.setAttribute("type", "button")
+    loginButton.innerHTML = 'Criar conta';
+    loginButton.onclick = function(){
+        alert("teste");
+      }
+
     var box = document.getElementById("registerBox")
     box.appendChild(registerArea)
+    registerArea.appendChild(namesBox)
+    registerArea.appendChild(form)
+    namesBox.appendChild(firstNameLabel)
+    form.appendChild(firstName)
+    namesBox.appendChild(secondNameLabel)
+    form.appendChild(secondName)
+    form.appendChild(emailLabel)
+    form.appendChild(email)
+    form.appendChild(passwordLabel)
+    form.appendChild(password)
+    form.appendChild(confirmPasswordLabel)
+    form.appendChild(confirmPassword)
+    form.appendChild(loginButton)
+    
+    let espera2 = await delay(750)
+    registerArea.style.opacity = "1"
 }
+
